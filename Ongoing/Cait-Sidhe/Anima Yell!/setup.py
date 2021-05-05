04568e0378310ff6f90eb90a47efff1477ae9f62
RivenSkaye <pretender0906@gmail.com>
2021-05-05 09:24:07 +0200

    Final package component

---
+++ Ongoing/Cait-Sidhe/Anima Yell!/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+name = "AY_NCs"
+version = "0.0.1"
+release = "0.0.1"
+
+setuptools.setup(
+    name=name,
+    version=release,
+    author="RivenSkaye",
+    author_email="imake@yomomma.com",
+    description="Anima Yell! NCs",
+    packages=["AY_NCs"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
