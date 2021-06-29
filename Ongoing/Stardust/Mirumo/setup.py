4be0851be141b045eba2dbf25c19b529db05dc97
RivenSkaye <pretender0906@gmail.com>
2021-06-29 17:30:01 +0200

    NC package setup

---
+++ Ongoing/Stardust/Mirumo/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+name = "Mirumo_NCs"
+version = "0.0.1"
+release = "0.0.1"
+
+setuptools.setup(
+    name=name,
+    version=release,
+    author="RivenSkaye",
+    author_email="imake@yomomma.com",
+    description="Mirumo NCs",
+    packages=["Mirumo_NCs"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
